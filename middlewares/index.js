68fbae67d377b3ba25d9aec7f65306b82d63144d
Basab Saha <134124512+Basab-Saha@users.noreply.github.com>
2023-10-26 16:39:22 +0530

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,1 +1,15 @@
+const fs = require("fs");
+
+function logReqRes(fileName) {
+    return (req, res, next) => {
+        fs.appendFile(fileName, `\n ${Date.now()} : ${req.method} : ${req.path} `, (err) => {
+            if (err) {
+                console.error("Error writing to log file:", err);
+            }
+            next(); // Call next() inside the callback to continue the middleware chain
+        });
+    }
+}
+
+module.exports = logReqRes; // Export the function directly, not wrapped in an object
 
